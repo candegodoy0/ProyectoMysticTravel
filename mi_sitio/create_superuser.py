62a35d57197d412846a68d467ea5b6cebff3aff9
Cande <godoycandela65@gmail.com>
2025-11-24 17:01:07 -0300

    Se modifican credenciales de acceso en create_superuser.py

--- mi_sitio/create_superuser.py
+++ mi_sitio/create_superuser.py
@@ -6,33 +6,13 @@ try:
 
     if not User.objects.filter(username="postgres").exists():
         User.objects.create_superuser(
-            username="candegodoy",
-            email="godoycandela65@gmail.com",
-            password="Cande1937"
+            username="godoycande",
+            email="candegody57@gmail.com",
+            password="Cande1937!"
         )
         print("Superusuario creado")
     else:
         print("El superusuario ya existe")
 
 except OperationalError:
-    from django.contrib.auth import get_user_model
-    from django.db.utils import OperationalError
-
-    USERNAME = "candegodoy"
-    EMAIL = "godoycandela65@gmail.com"
-    PASSWORD = "Cande1937"
-
-    try:
-        User = get_user_model()
-        if not User.objects.filter(username=USERNAME).exists():
-            User.objects.create_superuser(
-                username=USERNAME,
-                email=EMAIL,
-                password=PASSWORD
-            )
-            print(f"Superusuario '{USERNAME}' creado exitosamente.")
-        else:
-            print(f"El superusuario '{USERNAME}' ya existe. Se omite la creación.")
-
-    except OperationalError:
-        print("La base de datos aún no está lista. Intenta de nuevo tras migraciones.")
+    print("La base de datos aún no está lista. Intenta de nuevo tras migraciones.")
